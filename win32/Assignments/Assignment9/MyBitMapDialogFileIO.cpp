773dfb0a4cf171ec2b160759e2d1721178aa0650
bharatmazire <mazire.bharat@gmail.com>
2018-08-14 01:12:15 +0530

    changes done

--- win32/Assignments/Assignment9/MyBitMapDialogFileIO.cpp
+++ win32/Assignments/Assignment9/MyBitMapDialogFileIO.cpp
@@ -12,8 +12,8 @@
 #include <strsafe.h>
 
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-BOOL CALLBACK MyDlgProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc  (HWND, UINT, WPARAM, LPARAM);
+BOOL    CALLBACK MyDlgProc(HWND, UINT, WPARAM, LPARAM);
 
 typedef struct InputData
 {
@@ -28,20 +28,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	HWND hwnd;
 	MSG msg;
 
-	wndclass.cbClsExtra = 0;
-	wndclass.cbSize = sizeof(wndclass);
-	wndclass.cbWndExtra = 0;
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hInstance = hInstance;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.lpszClassName = szAppName;
-	wndclass.lpszMenuName = NULL;
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra		= 0;
+	wndclass.cbSize			= sizeof(wndclass);
+	wndclass.cbWndExtra		= 0;
+	wndclass.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm		= LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hInstance		= hInstance;
+	wndclass.lpfnWndProc	= WndProc;
+	wndclass.lpszClassName	= szAppName;
+	wndclass.lpszMenuName	= NULL;
+	wndclass.style			= CS_HREDRAW | CS_VREDRAW;
 
-	RegisterClassEx(&wndclass);
+	if (!RegisterClassEx(&wndclass))
+	{
+		MessageBox(NULL, TEXT("CLASS REGISTRATION FAILD !!"), TEXT("ERROR MESSAGE"), MB_OK | MB_ICONERROR);
+		return 0;
+	}
 
 	hwnd = CreateWindow(szAppName, TEXT("MY WINDOW WITH BITMAP"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
 
@@ -58,12 +62,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
-	static int siInitialSpace,siInitialMessage = 0;
-	static HBITMAP hBitMap;
-	HDC hdc, hdc1;
-	PAINTSTRUCT ps;
-	RECT rc;
-	BITMAP bmp;
+	static	int			siInitialSpace,siInitialMessage = 0;
+	static	HBITMAP		hBitMap;
+			HDC			hdc, hdc1;
+			PAINTSTRUCT ps;
+			RECT		rc;
+			BITMAP		bmp;
+			HFONT		hFont;
 
 	switch (iMsg)
 	{
@@ -72,16 +77,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		hBitMap = LoadBitmap((HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), MAKEINTRESOURCE(MY_BITMAP));
 		if (!hBitMap)
 		{
-			MessageBox(hwnd, TEXT("ERROR Loading Bitmap"), TEXT("ERROR MSG"), MB_OK);
+			MessageBox(hwnd, TEXT("ERROR Loading Bitmap"), TEXT("ERROR MSG"), MB_OK | MB_ICONERROR);
+			DestroyWindow(hwnd);
 		}
-		
 		break;
 
 	case WM_PAINT:
-		//MessageBox(hwnd, TEXT("IN PAINT"), TEXT("PAINT BOX"), MB_OK);
 		hdc = BeginPaint(hwnd, &ps);
 		GetClientRect(hwnd, &rc);
-		HFONT hFont;
 		hdc1 = CreateCompatibleDC(hdc);
 
 		SelectObject(hdc1, hBitMap);
@@ -89,20 +92,20 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		
 		SetStretchBltMode(hdc, HALFTONE);
 		StretchBlt(hdc, 0, 0, rc.right, rc.bottom, hdc1, 0, 0, bmp.bmWidth, bmp.bmHeight, SRCCOPY);
+		
 		if(siInitialMessage == 0)
 		{	
 			hFont = CreateFont(40, 0, 0, 0, FW_THIN, TRUE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
-				CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH | FF_ROMAN, TEXT("Impact"));
+				CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, TEXT("Arial"));
 
 			SelectObject(hdc, hFont);
 			
 			SetBkMode(hdc, TRANSPARENT);
-			SetTextColor(hdc, RGB(255, 255, 255));
-			DrawText(hdc, TEXT("Welcome !! \n Press Space bar to continue ..."), -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+			SetTextColor(hdc, RGB(0,0, 0));
+			DrawText(hdc, TEXT("\n\n\n\n\n\n\n\n\n\n\nWelcome !! \n\n\n Press Space bar to continue ..."), -1, &rc, DT_CENTER);
+			DeleteObject(hFont);
 		}
 		
-		//TextOut(hdc, 0, 0, TEXT("WELCOME"), 7);
-
 		DeleteDC(hdc1);
 		EndPaint(hwnd, &ps);
 		break;
@@ -113,22 +116,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		case VK_SPACE:
 			if (siInitialSpace == 0)
 			{
-				siInitialMessage = 1;
-				siInitialSpace = 1;
-				MessageBox(hwnd, TEXT("SPACE PRESSED !!"), TEXT("MESSAGE"), MB_OK);
+				siInitialMessage	= 1;
+				siInitialSpace		= 1;
+				
+				//MessageBox(hwnd, TEXT("SPACE PRESSED !!"), TEXT("MESSAGE"), MB_OK);
 				if (DialogBox((HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), MAKEINTRESOURCE(DATAENTRY), hwnd, (DLGPROC)MyDlgProc) == IDOK)
 				{
-					MessageBox(hwnd, TEXT("MSG"), TEXT("SUCCESS"), MB_OK);
+				//	MessageBox(hwnd, TEXT("MSG"), TEXT("SUCCESS"), MB_OK);
 				}
 				else
 				{
-					MessageBox(hwnd, TEXT("MSG"), TEXT("FAIL"), MB_OK);
+					MessageBox(hwnd, TEXT("Dialog Box call FAIL"), TEXT("ERROR MESSAGE"), MB_OK | MB_ICONERROR);
+					DestroyWindow(hwnd);
 				}
 				InvalidateRect(hwnd, FALSE, NULL);
 			}
 			break;
 		}
 		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -144,7 +150,7 @@ BOOL CALLBACK MyDlgProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	int fileHandle = 0;
 	//DWORD bytesWritten = 0;
 	unsigned    bytesWritten = 0;
-	HANDLE fhndl;
+	//HANDLE fhndl;
 
 
 	switch (iMsg)
@@ -174,6 +180,7 @@ BOOL CALLBACK MyDlgProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 			
 			EndDialog(hwnd, wParam);
 			return TRUE;
+		
 		case IDCANCEL:
 			EndDialog(hwnd, wParam);
 			return TRUE;
